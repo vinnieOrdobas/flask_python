ae1496b42941265ebcbfa1e8cf7a38e2e92a92ac
vinnieOrdobas <viniordobas@icloud.com>
2021-02-23 14:37:01 +0000

    about updated

--- run.py
+++ run.py
@@ -26,6 +26,6 @@ def careers():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"), 
-        port=int(os.environ.get("PORT","5000")), 
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT","5000")),
         debug=True)
